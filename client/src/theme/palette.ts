a797d697314ec837fb1020a7f42a1aeaad8201b0
jottaxwds <hello@jottax.xyz>
2023-03-17 02:26:39 +0100

    theming

---
+++ client/src/theme/palette.ts
@@ -0,0 +1,12 @@
+export default {
+  darkBlue: '#090B34',
+  midBlue: '#181D4D',
+  lightBlue: '#9898E1',
+  blue: '#6086E5',
+  light: '#C9D1D8',
+  green: '#00DC9C',
+  orange: '#FBC64A',
+  red: '#EF7980',
+  subdued: '#858599',
+  white: '#FFFFFF',
+} as const;
