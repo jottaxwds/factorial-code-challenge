2c89d5edaa55286ec7e3426653698b13fca2db45
jottaxwds <hello@jottax.xyz>
2023-03-19 06:24:57 +0100

    remove unused

--- client/src/components/List/LoadingItem.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from "./styles";
-
-const LoadingItem = ({ colSpan = 0 }) => (
-  <S.Loading data-testid={"loading-item"}>
-    <td colSpan={colSpan}>Loading...</td>
-  </S.Loading>
-);
-
-export default LoadingItem;
--- client/src/components/List/NoItems.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from "./styles";
-
-const NoItems = ({ colSpan = 0 }) => (
-  <S.NoItems data-testid={"no-items"}>
-    <td colSpan={colSpan}>...No items found...</td>
-  </S.NoItems>
-);
-
-export default NoItems;
